fe3ca41f79cdb3ee8617cf95ab4c7c129628ef04
NeverLeave2006 <1035445128@qq.com>
2021-08-20 13:48:53 +0800

    cpp001

---
+++ cpp001.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i=1;    //整形变量
+    cout<<"欢迎"<<endl;
+    cout<<'a'<<endl;
+    cout<<"i="<<i<<endl;
+    cout<<'\n';
+    return 0;
+}
